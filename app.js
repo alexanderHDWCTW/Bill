47c1794cf6831d799f01e9c4e1c1e1abb3d5d625
feishepherd <thomasshepherd2002@gmail.com>
2016-04-03 12:32:45 -0700

    front end apis

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var api = require('./routes/api');
 var app = express();
 
 // view engine setup
@@ -24,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,7 +32,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
--- public/javascripts/client-angular.js
+++ public/javascripts/client-angular.js
@@ -1,149 +1,36 @@
  (function(){                                                                     
 var app = angular.module('myApp', ['ngRoute']);
 
-
-app.config(function($routeProvider, $locationProvider) {
-  $routeProvider
-  .when('/', {
-      templateUrl: '../partials/games.html',
-      controller: 'gamesctrl'
-  }).when('/create', {
-      templateUrl: '../partials/create.html',
-      controller: 'createCtrl'
-  }).when('/games/:id', {
-      templateUrl: '../partials/game.html',
-      controller: 'active'
-  }).
-  otherwise({ redirectTo: '/'});
-  $locationProvider.html5Mode(true);
-});
-
-
-app.controller('gamesctrl', function($scope,$sce){
-  var tag, firstScriptTag, player;
-  if(typeof YT == 'undefined'){
-    tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+app.service('dataService', ['$http', function ($http) {
+  var urlBase = 'http://millerlister.com/MapSearchJSON?do=';
+  this.getFeatured = function () {
+      return $http.get(urlBase + 'MapSearchJSON?do=featured_listings');
+  };
+  this.getOffice = function () {
+      return $http.get(urlBase + 'MapSearchJSON?do=office_listings');
+  };
+  this.getMls = function () {
+      return $http.get(urlBase + 'MapSearchJSON?do=query_mls_disclaimers');
+  };
+  this.getCities = function () {
+      return $http.get(urlBase + 'mls_areas?mls=REIL&selectedCounty=SANTA+CLARA+CA&chosenAreas=&chosenCities=')
   }
+}]);
 
-  var inter = setInterval(function(){ 
-    if(typeof YT != 'undefined'){
-      player = new YT.Player('player', {
-      height: '100%',
-      width: '100%',
-      videoId: 'GjoX2LqOAuQ',
-      playerVars: {
-        autoPlay: 0
-      }
-     });
-     clearInterval(inter)
-    }
-  }, 500);
-
-  $scope.tldw = 
-    {
-      date:"25 October 2016",
-      videoId:"GjoX2LqOAuQ",
-      title:"TSM vs CLG",
-      highlights:[
-        {start:960,end:1000,text:'Doublelift getting tower dove'},
-        {start:1140,end:100,text:'4v3 team fight'},
-        {start:1915,end:50,text:'Hauntzer getting caught'},
-        {start:2068,end:80,text:'Team fight at 30 mins'},
-        {start:2194,end:300,text:'5v5 team fight'},
-        {start:2545,end:40,text:'YellowStar getting caught'},
-        {start:2740,end:40,text:'Game ending fight'},
-      ]
-    };
-
+app.controller('maincontroller', function($scope,$sce,dataService){
   
-  $scope.switchUrl = function(v){
-    player.seekTo($scope.tldw.highlights[v].start,true);
-    player.playVideo();
-  }
-  $scope.switchSrc = function(id){
-    if($scope.currentVideoId!=id){
-      player.loadVideoById(id);
-      $scope.currentVideoId = id;
-    }
-  }
 });
 
-app.controller('createCtrl', function($scope,$sce,$http){
-  var tag2, firstScriptTag2, player2;
-  tag2 = document.createElement('script');
-  tag2.src = "https://www.youtube.com/iframe_api";
-  firstScriptTag2 = document.getElementsByTagName('script')[0];
-  firstScriptTag2.parentNode.insertBefore(tag2, firstScriptTag2);
-  $scope.loaded = false;
-  $scope.arr = [];
-  function isInteger(n) {
-    return (typeof n == 'number' && /^-?\d+$/.test(n+''));
-  }
-  $scope.isDisabled = function(){
-    return !$scope.loaded;
-  }
-  $scope.addObj = function(){
-    var c,s,e;
-    c = $('#contextt').val();
-    s = parseInt( $('#startmin').val()*60)+parseInt($('#startsec').val() );
-    e = parseInt(($('#endmin').val()*60)+parseInt($('#endsec').val()));
-    if(isNaN(s) || isNaN(e)){
-      alert('Complete all fields')
-      return;
-    }
-    if(parseInt($('#startsec').val())>59 || parseInt($('#endsec').val())>59 ){
-      alert('Seconds cannot be greater than 59');
-      return;
-    }
-    if(c.length < 1){
-      alert('Complete context field');
-      return;
-    }
-    $scope.arr.push({
-      context:c,
-      start:s,
-      end:e
-    })
-  }
-  $scope.loadvid = function(){
-    var inter = setInterval(function(){ 
-    if(typeof YT != 'undefined'){
-      if(typeof player.c == 'undefined'){
-        player = new YT.Player('player', {
-        height: '100%',
-        width: '100%',
-        videoId: $('#videoidd').val(),
-        playerVars: {
-          autoPlay: 0
-        }
-       });
-      }else{
-        player.loadVideoById($('#videoidd').val());
-      }
-     clearInterval(inter)
-    }
-    }, 500);
-    $scope.loaded = true;
-  }
-  $scope.removeObj = function(i){
-    $scope.arr.splice(i,1)
-  }
-  $scope.playObj = function(v){
-    player.seekTo($scope.arr[v].start,true);
-    player.playVideo();
-  }
-  $scope.createTLDW = function(){
-    $http.post('/api/tldw', {
-      title: "title",
-      videoId: "id",
-      highlights:$scope.arr
-    }).then(function(data){
-      console.log(data.data)
-    });
-  }
+app.controller('featuredController', function($scope,$sce,dataService){
+ 
+});
+
+app.controller('officeController', function($scope,$sce,dataService){
+
+});
+
+app.controller('openHouseController', function($scope,$sce,dataService){
+
 });
 
 })();  
